dfd3840ee754192bbd4759b65d1424926a28481f
Yang Liu <yanliu@oradev.oraclecorp.com>
2018-06-25 02:35:50 -0600

    'add'

--- createtimecard.py
+++ createtimecard.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class MyDriver:
     def  __enter__(self):
